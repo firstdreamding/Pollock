07efec64af6d063bafea0d58a9d9000cf275931c
Geoffrey Ding <huaxu3721@gmail.com>
2020-11-12 14:33:43 +1100

    Added texture asset serialization

--- Sandbox/src/TextureEditor.cpp
+++ Sandbox/src/TextureEditor.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include "Pollock/Utils/FileDialogs.h"
 
+#include "Pollock/TextureSerializer.h"
+
 TextureEditor::TextureEditor()
 {
 	TextureProperties props = { TextureFilter::Nearest, TextureWrap::Clamp };
@@ -57,16 +59,55 @@ void TextureEditor::OnImGuiRender()
 	}
 	ImGui::EndGroup();
 
-	ImGui::Checkbox("Animation", &m_IsAnimation);
-	if (m_IsAnimation)
+	if (m_CurrentTexture)
 	{
-		ImGui::DragInt("Vertical Sprite Count", &m_VerticalSpriteCount);
-		ImGui::DragInt("Horizontal Sprite Count", &m_HorizontalSpriteCount);
-		if (ImGui::Button("Create Sub-Texture"))
-		{
-			m_AnimationTexture = std::make_shared<SubTexture2D>(m_CurrentTexture, m_VerticalSpriteCount, m_HorizontalSpriteCount);
-		}
-	}
+		ImGui::Separator();
 
+		const char* filteringTypeStrings[] = { "Linear", "Nearest" };
+		static const char* currentFilterType = filteringTypeStrings[0];
+		if (ImGui::BeginCombo("Filtering", currentFilterType))
+		{
+			for (int type = 0; type < 2; type++)
+			{
+				bool isSelected = (currentFilterType == filteringTypeStrings[type]);
+				if (ImGui::Selectable(filteringTypeStrings[type], isSelected))
+				{
+					currentFilterType = filteringTypeStrings[type];
+					// set texture filtering
+					m_CurrentTexture->SetFilter((TextureFilter)(type + 1));
+				}
+
+				if (isSelected)
+					ImGui::SetItemDefaultFocus();
+			}
+
+			ImGui::EndCombo();
+		}
+
+		ImGui::Separator();
+
+		ImGui::Checkbox("Animation", &m_IsAnimation);
+		if (m_IsAnimation)
+		{
+			ImGui::DragInt("Framerate", &m_Framerate);
+			ImGui::DragInt("Vertical Sprite Count", &m_VerticalSpriteCount);
+			ImGui::DragInt("Horizontal Sprite Count", &m_HorizontalSpriteCount);
+			if (ImGui::Button("Create Sub-Texture"))
+			{
+				m_AnimationTexture = std::make_shared<SubTexture2D>(m_CurrentTexture, m_VerticalSpriteCount, m_HorizontalSpriteCount);
+			}
+		}
+
+		if (ImGui::Button("Save As..."))
+		{
+			std::string filepath = FileDialogs::SaveFile("Pollock Texture(*.ptex)\0 *.ptex\0");
+			if (!filepath.empty())
+			{
+				TextureSerializer serializer(m_CurrentTexture, m_AnimationTexture);
+				serializer.Serialize(filepath);
+			}
+		}
+
+	}
 	ImGui::End();
 }
